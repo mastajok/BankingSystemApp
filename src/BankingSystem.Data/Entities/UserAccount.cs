dd3d9b86d6d78089fdd7de7992368c461dbd333d
Artem U <masta.jok88@gmail.com>
2023-03-14 07:39:02 +0100

    Initial app

---
+++ src/BankingSystem.Data/Entities/UserAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace BankingSystem.Data.Entities
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+        public decimal CurrentBalance { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ src/BankingSystem.DataAccess/BankingSystemDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BankingSystem.Data.Entities;
+using BankingSystem.DataAccess.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingSystem.DataAccess
+{
+    public class BankingSystemDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new UserAccountConfiguration());
+        }
+
+        public DbSet<User> Students { get; set; }
+        public DbSet<UserAccount> Courses { get; set; }
+    }
+}
